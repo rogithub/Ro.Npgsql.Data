ee0fa5586a9a76c5174e8a5e0cd378c37183835c
rodrigo debian <correo.rodrigo@gmail.com>
2025-09-07 11:22:53 -0500

    modificando interfaces

--- Ro.Npgsql.Data/IDbAsync.cs
+++ Ro.Npgsql.Data/IDbAsync.cs
@@ -12,9 +12,9 @@ namespace Ro.Npgsql.Data
         Task<int> ExecuteNonQuery(DbCommand cmd);
         Task<object> ExecuteScalar(DbCommand cmd);
         Task ExecuteReader(DbCommand cmd, Action<IDataReader> mapper, CommandBehavior behavior = CommandBehavior.CloseConnection);
-        Task<T> GetOneRow<T>(DbCommand cmd, Func<IDataReader, T> mapper);
+        Task<T?> GetOneRow<T>(DbCommand cmd, Func<IDataReader, T> mapper);
         Task<IEnumerable<T>> GetRows<T>(DbCommand cmd, Func<IDataReader, T> mapper);
-        Task<T> GetOneRowAsync<T>(DbCommand cmd, Func<IDataReader, Task<T>> mapper);
+        Task<T?> GetOneRowAsync<T>(DbCommand cmd, Func<IDataReader, Task<T>> mapper);
         Task<IEnumerable<T>> GetRowsAsync<T>(DbCommand cmd, Func<IDataReader, Task<T>> mapper);
     }
 }
